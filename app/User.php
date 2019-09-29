808a5486dad3b2e520f5889c1624599dba0a7ce8
awais512 <raz.awais25@gmail.com>
2019-09-29 05:11:57 -0700

    Question Schema and Model Created

--- app/User.php
+++ app/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use App\Question;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions() 
+    {
+        return $this->hasMany(Question::class);
+    }
 }
